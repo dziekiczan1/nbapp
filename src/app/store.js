a971adba651b9a7accf0852e018c018169f264ca
Piotr Rzadkowolski <p.rzadkowolski@gmail.com>
2022-07-14 21:53:07 +0200

    styles for news fetching added

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { nbaDataApi } from "../services/nbaDataApi";
+import { nbaNewsApi } from "../services/nbaNewsApi";
 
 export default configureStore({
   reducer: {
     [nbaDataApi.reducerPath]: nbaDataApi.reducer,
+    [nbaNewsApi.reducerPath]: nbaNewsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(nbaDataApi.middleware),
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,27 +1,62 @@
 import React, { useState } from "react";
+import { useGetTeamsQuery } from "../services/nbaDataApi";
 import { useGetNbaNewsQuery } from "../services/nbaNewsApi";
 
 const Home = () => {
-  const [category, setCategory] = useState("Cryptocurrency");
+  const [category, setCategory] = useState("Atlanta Hawks");
+  const { data: teams } = useGetTeamsQuery();
   const { data: nbaNews } = useGetNbaNewsQuery({
     category,
-    count: 20,
+    count: 10,
   });
 
+  const noImage = "https://piotr.rzadkowolski.dev/noimage.png";
+
   if (!nbaNews?.value) return "Loading...";
+
   console.log(nbaNews);
+
   return (
     <>
-      <div>
+      <div className="py-4 px-8">
         <select
-          placeholder="News"
-          onChange={(value) => setCategory(value)}
-          filterOption={(input, option) =>
-            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-          }
+          placeholder="Choose category"
+          onChange={(e) => setCategory(e.target.value)}
+          className="border-2 w-1/4 rounded shadow-md p-2"
         >
-          <option value="News">News</option>
+          <option value="Nba">Nba News</option>
+          {teams?.data.map((team) => (
+            <option value={team.full_name} className="hover:bg-sky-700">
+              {team.full_name}
+            </option>
+          ))}
         </select>
+      </div>
+      <div className="flex flex-col flex-wrap shadow-md bg-[#FEFBE7] cursor-pointer gap-12 px-8 py-4 w-1/2 mt-4">
+        {nbaNews.value.map((news, i) => (
+          <ul key={i}>
+            <li>
+              <div className="flex flex-row">
+                <img
+                  src={news?.image?.thumbnail?.contentUrl || noImage}
+                  alt={news.name}
+                  style={{ width: "100px", height: "100px" }}
+                  className="mr-2 shadow-xl rounded-lg"
+                />
+                <div className="flex flex-col">
+                  <a href={news.url}>
+                    <p className="font-bold">{news.name}</p>
+                    <p>
+                      {news.description.length > 200
+                        ? `${news.description.substring(0, 200)}...`
+                        : news.description}
+                    </p>
+                  </a>
+                </div>
+              </div>
+            </li>
+          </ul>
+        ))}
       </div>
     </>
   );
