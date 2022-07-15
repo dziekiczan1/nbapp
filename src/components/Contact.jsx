8132c9092c116d6571bf623f4794d48f93ececd9
Piotr Rzadkowolski <p.rzadkowolski@gmail.com>
2022-07-15 23:15:47 +0200

    added contact component with working form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+
+const FORM_ENDPOINT = process.env.REACT_APP_KEY_FORM_ENDPOINT;
 
 const Contact = () => {
-  return <div>Contact</div>;
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = () => {
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 100);
+  };
+
+  if (submitted) {
+    return (
+      <>
+        <div className="text-2xl">Thank you!</div>
+        <div className="text-md">We'll be in touch soon.</div>
+      </>
+    );
+  }
+
+  return (
+    <form
+      action={FORM_ENDPOINT}
+      onSubmit={handleSubmit}
+      method="POST"
+      target="_blank"
+      className="w-1/2 shadow-md bg-[#FEFBE7] p-4"
+    >
+      <div className="m-3">
+        <input
+          type="text"
+          placeholder="Your name"
+          name="name"
+          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          required
+        />
+      </div>
+      <div className="m-3">
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          required
+        />
+      </div>
+      <div className="m-3">
+        <textarea
+          placeholder="Your message"
+          name="message"
+          className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+          required
+        />
+      </div>
+      <div className="m-3 flex justify-center items-center">
+        <button
+          className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+          type="submit"
+        >
+          Send a message
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default Contact;
