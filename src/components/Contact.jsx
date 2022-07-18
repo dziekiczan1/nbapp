4bf4e366873ec0034c85872da6a315af6e6e3c11
dzekiczan1 <p.rzadkowolski@gmail.com>
2022-07-18 23:30:36 +0200

    fixed styling in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,7 +19,7 @@ const Contact = () => {
 
   return (
     <div className="flex flex-col lg:flex-row">
-      <div className="w-full lg:w-1/2 px-0 py-0 md:py-4 md:px-8">
+      <div className="w-full lg:w-1/2 px-0 py-0 md:py-4 md:px-4">
         {submitted ? (
           <div className="flex flex-col justify-center items-center">
             <p className="text-center text-2xl md:text-5xl font-bold mb-4 text-[#d13c1b]">
@@ -41,7 +41,7 @@ const Contact = () => {
             </p>
           </div>
         )}
-        <div className="shadow-md bg-[#FEFBE7] py-4 px-8 h-full">
+        <div className="shadow-md bg-[#FEFBE7] py-4 px-8">
           <img src={mail} alt="Contact" className="mt-4 mb-8 mx-auto" />
           <form action={FORM_ENDPOINT} onSubmit={handleSubmit} method="POST">
             <div className="mx-0 my-3 md:m-3">
@@ -73,7 +73,7 @@ const Contact = () => {
             </div>
             <div className="m-3 flex justify-center items-center">
               <button
-                className="bg-[#F9EBC8] text-[#d13c1b] active:bg-[#fdf6c7] font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg hover:bg-[#fdf6c7] outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                className="bg-[#F9EBC8] text-[#d13c1b] active:bg-[#fdf6c7] font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg hover:bg-[#fdf6c7] outline-none focus:outline-none mr-1 mb-4 ease-linear transition-all duration-150"
                 type="submit"
               >
                 Send a message
@@ -82,7 +82,7 @@ const Contact = () => {
           </form>
         </div>
       </div>
-      <div className="w-full lg:w-1/2 px-0 pt-4 md:py-4 md:px-8">
+      <div className="w-full lg:w-1/2 px-0 pt-4 md:py-4 md:px-4">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2441.814214988353!2d21.038355615946145!3d52.26491866285378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ecc3cd0f071f3%3A0x5cdc9811aece7688!2sKamienna%201%2C%2003-441%20Warszawa!5e0!3m2!1spl!2spl!4v1657982505704!5m2!1spl!2spl"
           style={{ height: "450px", width: "100%" }}
